9de3dbd0f067f70677a51d66f135586df5373db6
I-Program-C-Plus-Plus <59405071+I-Program-C-Plus-Plus@users.noreply.github.com>
2020-01-20 20:53:24 -0500

    Create searchfile.py
    This is a module I made for Python.

---
+++ searchfile.py
@@ -0,0 +1,37 @@
+import glob
+import os
+
+#Author Stephen Nobilio
+
+class File:
+
+    def findFileByType(self,typeOfFile,directory):
+        os.chdir(directory)
+        files = []
+        for file in glob.glob("*.{}".format(typeOfFile)):
+            files.append(file)
+        return files
+
+    def findFileByName(self,nameOfFile,directory):
+        os.chdir(directory)
+        files = []
+        for file in glob.glob("{}.*".format(nameOfFile)):
+            files.append(file)
+        return files
+
+    def deleteFileByType(self,typeOfFile,directory):
+        os.chdir(directory)
+        for file in glob.glob("*.{}".format(typeOfFile)):
+            os.remove(file)
+
+    def deleteFileByName(self,nameOfFile,directory):
+        os.chdir(directory)
+        for file in glob.glob("{}.*".format(nameOfFile)):
+            os.remove(file)
+            
+    def makeFilesByType(self,nameOfFile,typeOfFile,directory,amountOfFiles):
+        os.chdir(directory)
+        for i in range(0,amountOfFiles):
+            file = open("{}({}).{}".format(nameOfFile,i,typeOfFile),"w")            
+
+file = File()
